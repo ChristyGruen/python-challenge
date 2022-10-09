ed23dc6a23582cdd6caf0a0166bb9f863c6ded44
ChristyGruen <chrisgruen@yahoo.com>
2022-10-09 12:15:37 -0500

    PyBank homework begin

---
+++ PyBank/main.py
@@ -0,0 +1,14 @@
+def PyBankRecords():
+    #Total number of months
+        # NMonths = loop to create list of months and count list length
+    #Net total of Profit 
+        # TotProfit =  loop to add profit/loss to total profit counter
+    #Profit/losses change 
+        # AvePL = loop to calculate change by month, create profit loss change list, calculate sum of list divided by length of list to get average
+    #greatest increase in profits
+        # MaxPL = max profit loss change list value and month
+    #greatest decrease in profits
+        # MinPL = min profit loss change list value and month
+    # print header "Financial Analysis"
+    # print separator "-------"
+    # print with titles: NMonths, TotProfit, AvePL, MaxPL, MinPL
